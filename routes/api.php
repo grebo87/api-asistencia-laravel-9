3baad987134402521ab2d4fd130e7a0adfbbe14e
grebo <grebodeveloper@gmail.com>
2023-02-10 03:58:39 -0400

    login, logout, get user

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AssistanceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PersonalController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +17,36 @@ use App\Http\Controllers\Api\PersonalController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('personal/{personal_id}/get-assistance', [AssistanceController::class, 'getStaffDayAttendance'])->name('personal.get-assistance');
-Route::post('personal/{personal_id}/mark-start-time', [AssistanceController::class, 'markStartTime'])->name('personal.mark-start-time');
-Route::put('personal/{personal_id}/mark-time-of', [AssistanceController::class, 'markTimeOf'])->name('personal.mark-time-of');
 
-Route::get('personal/{personal_id}/get-absences', [AbsenceController::class, 'getAbsenceByPersonalId'])->name('personal.get-absences');
-Route::get('personal/{personal_id}/verify-absences', [AbsenceController::class, 'verifyAbsenceByPersonalId'])->name('personal.verify-absences');
-Route::post('personal/{personal_id}/store-absences', [AbsenceController::class, 'store'])->name('personal.store-absences');
-Route::put('personal/{personal_id}/update-absences', [AbsenceController::class, 'update'])->name('personal.update-absences');
-Route::delete('personal/{personal_id}/destroy-absences', [AbsenceController::class, 'destroy'])->name('personal.destroy-absences');
 
-Route::apiResource('personal', PersonalController::class);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('personal/{personal_id}/get-assistance', [AssistanceController::class, 'getStaffDayAttendance'])->name('personal.get-assistance');
+    Route::post('personal/{personal_id}/mark-start-time', [AssistanceController::class, 'markStartTime'])->name('personal.mark-start-time');
+    Route::put('personal/{personal_id}/mark-time-of', [AssistanceController::class, 'markTimeOf'])->name('personal.mark-time-of');
+    
+    Route::get('personal/{personal_id}/get-absences', [AbsenceController::class, 'getAbsenceByPersonalId'])->name('personal.get-absences');
+    Route::get('personal/{personal_id}/verify-absences', [AbsenceController::class, 'verifyAbsenceByPersonalId'])->name('personal.verify-absences');
+    Route::post('personal/{personal_id}/store-absences', [AbsenceController::class, 'store'])->name('personal.store-absences');
+    Route::put('personal/{personal_id}/update-absences', [AbsenceController::class, 'update'])->name('personal.update-absences');
+    Route::delete('personal/{personal_id}/destroy-absences', [AbsenceController::class, 'destroy'])->name('personal.destroy-absences');
+    
+    Route::apiResource('personal', PersonalController::class);
+
+    Route::get('user', UserController::class);
+
+});
+
+
 
 Route::get('create_personal_fake', function(){
     App\Models\Personal::factory(10)->create();
+
+    // \App\Models\User::factory()->create([
+    //     'name' => fake()->name(),
+    //     'email' => fake()->unique()->safeEmail(),
+    //     'email_verified_at' => now(),
+    //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    //     'remember_token' => 'jewgjfknewgflne.fj',
+    // ]);
+
 });
